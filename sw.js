bb3b03ba1c49f1dc95353cf23112c5d92c1d9c09
Riddsaw <axel.deshayes.pro@gmail.com>
2019-11-20 15:26:54 +0100

    ajout sw.js

---
+++ sw.js
@@ -0,0 +1,50 @@
+var cacheName = '';
+var contentToCache =["./index.html",
+"./manifest.json",
+"./produits.csv",
+"./style.css",
+"./scanettePWA.zip",
+"./favicon",
+"./images/barcode-scanner.png",
+"./images/icon-cart.png",
+"./images/icon-setup.png",
+"./images/icon-transmit.png",
+"./images/logo.png",
+"./icons/icon-32.png",
+"./icons/icon-64.png",
+"./icons/icon-96.png",
+"./icons/icon-128.png",
+"./icons/icon-168.png",
+"./icons/icon-180.png",
+"./icons/icon-192.png",
+"./icons/icon-256.png",
+"./icons/icon-512.png",
+"./js/app.js",
+"./js/DecoderWorker.js",
+"./js/exif.js",
+"./js/job.js"
+];
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+      caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all: app shell and content');
+        return cache.addAll(contentToCache);
+      })
+    );
+  });
+  self.addEventListener('fetch', (e) => {
+    e.respondWith(
+      caches.match(e.request).then((r) => {
+            console.log('[Service Worker] Fetching resource: '+e.request.url);
+        return r || fetch(e.request).then((response) => {
+                  return caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching new resource: '+e.request.url);
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        });
+      })
+    );
+  });
